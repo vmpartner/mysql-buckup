8597cc0ac8bbc4b7143ed6639438a8fee628d376
vitams <vmpartner@gmail.com>
2021-08-24 04:05:55 +0500

    Up

--- backup.sh
+++ backup.sh
@@ -3,14 +3,20 @@
 set -e
 
 DATETIME=$(date +"%s_%Y-%m-%d")
-NAME="${DT}.gz"
+NAME="${DATETIME}.gz"
 
+echo "Start dump ${$NAME}"
 /usr/bin/mysqldump --single-transaction --quick --lock-tables=false -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} | gzip > "/backup/${NAME}"
+
+echo "Start copy"
 rclone copy "/backup/${NAME}" "$RCLONE_DEST"
 rm -rf "/backup/${NAME}"
 
+echo "Curl check url"
 if [ "$CHECK_URL" = "**None**" ]; then
   echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor sync job"
 else
   curl "$CHECK_URL"
 fi
+
+echo "Done"
